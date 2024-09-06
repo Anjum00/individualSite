87acd85f94d0c436f19bcec1a346d27bf568cd8c
Anjum00 <105652255+Anjum00@users.noreply.github.com>
2024-09-06 16:19:54 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/individualSite/', // Set the correct base path for deployment (e.g., to GitHub Pages)
   server: {
-    port: 3000, // Specify your desired port number here
+    port: 3000, // Specify your desired port number for the development server
   },
-})
+});
